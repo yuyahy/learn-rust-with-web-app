d7f8aff293a933bb6dcbe63063942aac5fc3ab35
yuyahy <xvto.ya@gmail.com>
2026-02-28 04:58:10 +0900

    feat: P97: loggingまで

---
+++ my-todo/src/main.rs
@@ -0,0 +1,23 @@
+use axum::{routing::get, Router};
+use std::env;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // loggingの初期化
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hellow, world!"
+}
