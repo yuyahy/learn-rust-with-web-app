7e98c25129bc383734fb3a0e4200fa4385c7e831
yuyahy <xvto.ya@gmail.com>
2026-05-27 05:25:05 +0900

    feat: P257: LabelRepositoryForDbまで

--- my-todo/src/repositories.rs
+++ my-todo/src/repositories.rs
@@ -9,4 +9,6 @@ enum RepositoryError {
     Unexpected(String),
     #[error("Not Found, id is {0}")]
     NotFound(i32),
+    #[error("Duplicate data, id is {0}")]
+    Duplicate(i32),
 }
--- my-todo/src/repositories/label.rs
+++ my-todo/src/repositories/label.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code, unused_variables)]
 
+use super::RepositoryError;
 use axum::async_trait;
 use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
@@ -37,15 +38,60 @@ impl LabelRepositoryForDb {
 #[async_trait]
 impl LabelRepository for LabelRepositoryForDb {
     async fn create(&self, name: String) -> anyhow::Result<Label> {
-        todo!()
+        let optional_label = sqlx::query_as::<_, Label>(
+            r#"
+            select * from labels where name = $1
+            "#,
+        )
+        .bind(name.clone())
+        .fetch_optional(&self.pool)
+        .await?;
+
+        if let Some(label) = optional_label {
+            return Err(RepositoryError::Duplicate(label.id).into());
+        }
+
+        let label = sqlx::query_as::<_, Label>(
+            r#"
+            insert into labels ( name )
+            values ( $1 )
+            returning *
+            "#,
+        )
+        .bind(name.clone())
+        .fetch_one(&self.pool)
+        .await?;
+
+        Ok(label)
     }
 
     async fn all(&self) -> anyhow::Result<Vec<Label>> {
-        todo!()
+        let labels = sqlx::query_as::<_, Label>(
+            r#"
+            select * from labels order by labels.id asc
+            "#,
+        )
+        .fetch_all(&self.pool)
+        .await?;
+
+        Ok(labels)
     }
 
     async fn delete(&self, id: i32) -> anyhow::Result<()> {
-        todo!()
+        sqlx::query(
+            r#"
+            delete from labels where id=$1
+            "#,
+        )
+        .bind(id)
+        .execute(&self.pool)
+        .await
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => RepositoryError::NotFound(id),
+            _ => RepositoryError::Unexpected(e.to_string()),
+        })?;
+
+        Ok(())
     }
 }
 
